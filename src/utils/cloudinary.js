4c2ff6e132eac5ac47e44c99eaaf62c5a250f89b
Priya-Verma988 <iampriyaverma01@gmail.com>
2026-02-20 19:35:11 +0530

    add multer and cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import {v2 as cloudinary} from "cloudinary" ;
+import fs from "fs";
+
+cloudinary.config({ 
+  cloud_name:process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key:process.env.CLOUDINARY_API_KEY , 
+  api_secret:process.env.CLOUDINARY_API_SECRET 
+});
+
+
+const uploadOncloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null //upload the file cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        //file has been uploaded successfullly
+        console.log("file is uploaded on cloudinary", response.url);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath)// remove the locally saved filed as the upload operation got failed
+
+        return null;
+    }
+}
+
+// cloudinary.v2.uploader.upload("https://res.cloudinary.com/dbqxwiqwz/image/upload/v1771422644/main-sample.png", { public_id: "main_sample"}, function(error, result)  {console.log(result); });
+
+export {uploadOncloudinary};
