f93b62e07f068b2e494210408e110bd5ef6434e3
Priya-Verma988 <iampriyaverma01@gmail.com>
2026-04-02 22:37:33 +0530

    debug user.router

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -41,7 +41,7 @@ router.route("/change-password").post(verifyJWT, changeCurrentPassword)
 router.route("/current-user").get(verifyJWT, getCurrentUser)
 router.route("/update-account").get(verifyJWT, updateAccountDetails)
 router.route("/update-avater").patch(verifyJWT, upload.single("avater"),  updateUserAvatar)
-router.route("/cover-image").patch(verifyJWT, upload.single("/coverImage"),updateUserCoverImage)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"),updateUserCoverImage)
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
 router.route("/history").get(verifyJWT, getWatchHistory)
 
