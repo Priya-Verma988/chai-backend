01908ebb0ff1c73d5eed88c20d72698541eaed1c
Priya-Verma988 <iampriyaverma01@gmail.com>
2026-04-05 13:00:10 +0530

    remove next function from user.models.js

--- src/index.js
+++ src/index.js
@@ -11,11 +11,11 @@ import {app} from "./app.js";
 dotenv.config({
     path: './.env'
 })
-
+const PORT = process.env.PORT || 8000
 connectDB()
 .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-        console.log(`Server is running at port ${process.env.PORT}`);
+    app.listen(PORT, () => {
+        console.log(`Server is running at port ${PORT}`);
         
     })
 })
